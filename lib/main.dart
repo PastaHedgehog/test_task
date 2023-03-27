a37c3d6d6dffae25cf72e8a93def0061e6ab413c
Galeev Timur <ybrf0711@gmail.com>
2023-03-27 12:02:40 +0500

    добавлен дизайн

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Currency Exchange Rates',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Currency Exchange Rates'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30)),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 10,
+          title: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Currency  Rates App',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 128, 128, 128),
+                  fontSize: 25,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
         ),
         body: FutureBuilder<List<Currency>>(
           //* создаем в нашем body виджет  FutureBuilder
@@ -22,7 +38,7 @@ class MyApp extends StatelessWidget {
           builder: //* приступаем к формированию нашего элемента
               (BuildContext context, AsyncSnapshot<List<Currency>> snapshot) {
             if (snapshot.hasData) {
-              //* проверка на наличие данных
+              //* Если у нас есть данные, то загрузим наш экран
               return ListView.builder(
                 //* создаем бесконечный ListView
                 itemCount: snapshot.data!
@@ -30,11 +46,41 @@ class MyApp extends StatelessWidget {
                 itemBuilder: (BuildContext context, int index) {
                   final currency = snapshot.data![
                       index]; //* получаем данные из предоставленного списка по индексу
-                  return Card(
-                    //* возвращаем элемент с нашими данными
-                    child: ListTile(
-                      title: Text(currency.name),
-                      subtitle: Text(currency.value.toStringAsFixed(2)),
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 30),
+                    child: SizedBox(
+                      height: 120,
+                      child: Card(
+                        //* возвращаем элемент с нашими данными
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            side: BorderSide(
+                                color: Color.fromRGBO(107, 107, 107, 1),
+                                width: 2)),
+                        elevation: 2,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: ListTile(
+                            title: Text(
+                              currency.name,
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "Roboto",
+                              ),
+                            ),
+                            trailing: Text(
+                              currency.value.toStringAsFixed(2) + " руб",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "Roboto",
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   );
                 },
